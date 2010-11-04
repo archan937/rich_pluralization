1997724bcd8f3b03e75493841e5d8cb98ca20a7e
Wilco van Duinkerken <wilco@holder.nl>
2010-11-04 20:52:28 +0100

    Further fixed the test suite

--- test/core/string/inflections_test.rb
+++ test/core/string/inflections_test.rb
@@ -1,15 +1,16 @@
 require File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "test_helper.rb"))
 
 class InflectionsTest < Test::Unit::TestCase
-  # setup do
-  #   I18n.locale = Engine.init(Locales::NL)
-  # end
-  # 
-  # test "truth" do
-  #   assert true
-  # end
+
+  def setup
+    I18n.locale = Engine.init(Locales::NL)
+  end
   
   def test_the_truth
     assert true
+  end
+  
+  def test_if_setup_works
+    assert_equal I18n.locale, :nl
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,7 @@ require "active_support"
 require "test/unit"
 require "rich_pluralization"
 require File.join(File.dirname(__FILE__), "..", "lib", "rich_pluralization")
+require "locales/nl/inflections_test"
 
 # 
 # class ActiveSupport::TestCase
@@ -15,3 +16,5 @@ require File.join(File.dirname(__FILE__), "..", "lib", "rich_pluralization")
 # 
 #   # Add more helper methods to be used by all tests here...
 # end
+
+include Rich::Pluralization
