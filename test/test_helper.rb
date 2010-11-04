facf4a81a5357fa18d947a9b61dd0eea2f062b44
Wilco van Duinkerken <wilco@holder.nl>
2010-11-04 21:11:11 +0100

    cleaned test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,19 +1,7 @@
 require "rubygems"
-require "active_support"
-# require "active_support/test_case"
 require "test/unit"
-require "rich_pluralization"
+require "active_support"
+
+
 require File.join(File.dirname(__FILE__), "..", "lib", "rich_pluralization")
-
-# 
-# class ActiveSupport::TestCase
-#   # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
-#   #
-#   # Note: You'll currently still have to declare fixtures explicitly in integration tests
-#   # -- they do not yet inherit this setting
-#   # fixtures :all
-# 
-#   # Add more helper methods to be used by all tests here...
-# end
-
 include Rich::Pluralization
