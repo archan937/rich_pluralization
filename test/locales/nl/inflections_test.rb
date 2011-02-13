92ce7674d90ed2bb618c4bd6c073c717d68651f8
Paul Engel <paul.engel@holder.nl>
2011-02-13 05:58:15 +0100

    Cleaned up requirements during testing

--- test/locales/nl/inflections_test.rb
+++ test/locales/nl/inflections_test.rb
@@ -1,4 +1,5 @@
 require File.expand_path("../../../test_helper.rb", __FILE__)
+require "csv"
 
 module Locales
   module NL
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,6 @@
 require "rubygems"
-require "test/unit"
 require "active_support"
 require "shoulda"
-require "csv"
 
 require File.expand_path("../../lib/rich_pluralization", __FILE__)
 include Rich::Pluralization
