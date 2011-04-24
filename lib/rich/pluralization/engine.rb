5b4839d4b8183d1e0394b8822bc268f25931587a
Paul Engel <paul.engel@holder.nl>
2011-04-24 11:37:21 +0200

    Using Rich::Support.after_initialize for configuring i18n

--- lib/rich/pluralization/engine.rb
+++ lib/rich/pluralization/engine.rb
@@ -1,35 +1,46 @@
 module Rich
   module Pluralization
-    module Engine
-      extend self
+    class Engine < Rails::Engine
 
-      def init(test_locale = nil)
-        if test_locale
-          I18n.load_path  =    [File.expand_path("../../../../locales/#{test_locale}.yml", __FILE__)]
-        else
-          I18n.load_path += Dir[File.expand_path("../../../../locales/*.yml"             , __FILE__)]
-        end
+      class << self
 
-        I18n.backend.reload!
-        Rich::Pluralization::Inflector.inflections.reset_attrs if test_locale
-
-        initial_locale = I18n.locale
-
-        I18n.backend.available_locales.each do |locale|
-          I18n.locale = locale
-
-          Rich::Pluralization::Inflector.inflections locale do |inflections|
-            (I18n.t! "e9s" rescue []).each do |type, entries|
-              entries.each do |inflection|
-                inflections.send *[type, inflection].flatten
-              end
-            end
+        def init(test_locale = nil)
+          Rich::Support.after_initialize do
+            config_i18n test_locale
           end
         end
 
-        I18n.locale = initial_locale
+      private
 
-        test_locale
+        def config_i18n(test_locale = nil)
+          if test_locale
+            I18n.load_path  =    [File.expand_path("../../../../locales/#{test_locale}.yml", __FILE__)]
+          else
+            I18n.load_path += Dir[File.expand_path("../../../../locales/*.yml"             , __FILE__)]
+          end
+
+          I18n.backend.reload!
+          Rich::Pluralization::Inflector.inflections.reset_attrs if test_locale
+
+          initial_locale = I18n.locale
+
+          I18n.backend.available_locales.each do |locale|
+            I18n.locale = locale
+
+            Rich::Pluralization::Inflector.inflections locale do |inflections|
+              (I18n.t! "e9s" rescue []).each do |type, entries|
+                entries.each do |inflection|
+                  inflections.send *[type, inflection].flatten
+                end
+              end
+            end
+          end
+
+          I18n.locale = initial_locale
+
+          test_locale
+        end
+
       end
 
     end
